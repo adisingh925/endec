a4887d04b3ac50a83898229fa8bfa3f257c5de13
Aditya <adisingh925@gmail.com>
2023-05-23 11:42:49 +0530

    encryption implemented

--- app/src/main/java/app/adreal/endec/Fragments/MainFragment.kt
+++ app/src/main/java/app/adreal/endec/Fragments/MainFragment.kt
@@ -1,12 +1,19 @@
 package app.adreal.endec.Fragments
 
+import android.app.Activity
+import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.OpenableColumns
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.MimeTypeMap
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import app.adreal.endec.R
@@ -16,12 +23,20 @@ import app.adreal.endec.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
+    companion object {
+        const val PICKFILE_REQUEST_CODE = 10
+    }
+
     private val binding by lazy {
         FragmentMainBinding.inflate(layoutInflater)
     }
 
-    private val mainViewModel by lazy{
+    private val mainViewModel by lazy {
         ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    private val contentResolver by lazy {
+        context?.contentResolver
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -32,7 +47,7 @@ class MainFragment : Fragment() {
 
         mainViewModel.generateArgonBasedAesKey()
 
-        mainViewModel.onKeyChange.observe(viewLifecycleOwner){
+        mainViewModel.onKeyChange.observe(viewLifecycleOwner) {
             binding.key.text = it
         }
 
@@ -40,6 +55,44 @@ class MainFragment : Fragment() {
             findNavController().navigate(R.id.action_mainFragment_to_settingsFragment)
         }
 
+        binding.encrypt.setOnClickListener {
+            openExplorer()
+        }
+
         return binding.root
+    }
+
+    private fun openExplorer() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        startActivityForResult(intent, PICKFILE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == PICKFILE_REQUEST_CODE) {
+            data?.data?.also { uri ->
+                Log.d("MIME Type", contentResolver?.getType(uri).toString())
+                dumpImageMetaData(uri)
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun dumpImageMetaData(uri: Uri) {
+        val cursor: Cursor? = contentResolver?.query(uri, null, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val displayName: String =
+                    it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                Log.d("File Data", "Display Name: $displayName")
+                val sizeIndex: Int = it.getColumnIndex(OpenableColumns.SIZE)
+                val size: String = if (!it.isNull(sizeIndex)) {
+                    it.getString(sizeIndex)
+                } else {
+                    "Unknown"
+                }
+                Log.d("File data", "Size: $size")
+            }
+        }
     }
 }
